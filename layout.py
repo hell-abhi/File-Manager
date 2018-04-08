3794ad4b95bf94a649c46cf3d5b3883fecbe29f7
Abhishek Keshri <akeshari87@gmail.com>
2018-04-08 07:49:04 +0530

    Implemented List View

--- layout.py
+++ layout.py
@@ -1,6 +1,9 @@
 import gi
+import os
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, GObject
+from gi.repository import Gtk, GObject, GdkPixbuf
+
+CURRENT_DIRECTORY = '/'
 
 class EntryWindow(Gtk.Window):
 
@@ -42,17 +45,95 @@ class EntryWindow(Gtk.Window):
         vertical_box.pack_start(tool_bar, False, False, 0)
         
         horizontal_box = Gtk.Box()
-        vertical_box.pack_start(horizontal_box, True, True, 0)
+        vertical_box.pack_start(horizontal_box, False, True, 0)
         
         # side bar
         placessidebar = Gtk.PlacesSidebar()
         placessidebar.connect("open-location", self.on_open_location)
-        horizontal_box.pack_start(placessidebar, True, True, 0)
+        horizontal_box.pack_start(placessidebar, True, False, 0)
+        horizontal_box.set_halign(1.0)
+        
+        # creating list view
+        store = Gtk.ListStore(GdkPixbuf.Pixbuf, str, bool, float, float)
+        self.get_files(store)
+        treeview = Gtk.TreeView(store)
+        treeview.connect("button_press_event", self.on_button1_clicked)
+        horizontal_box.pack_start(treeview, True, True, 0)
+
+        cellrenderertext = Gtk.CellRendererText()
+        cellrendererpixbuf = Gtk.CellRendererPixbuf()
+
+        #column 0 of list view to display icons
+        treeviewcolumn = Gtk.TreeViewColumn("Icon")
+        treeviewcolumn.set_spacing(10)
+        treeview.append_column(treeviewcolumn)
+        treeviewcolumn.pack_start(cellrendererpixbuf, True)
+        treeviewcolumn.add_attribute(cellrendererpixbuf, "pixbuf", 0)
+
+        #column 1 of list view to display names
+        treeviewcolumn = Gtk.TreeViewColumn("Name")
+        treeviewcolumn.set_spacing(100)
+        treeview.append_column(treeviewcolumn)
+        treeviewcolumn.pack_start(cellrenderertext, True)
+        treeviewcolumn.add_attribute(cellrenderertext, "text", 1)
+        
+        #column 2 of list view to display if it is directory
+        treeviewcolumn = Gtk.TreeViewColumn("Is Dir?")
+        treeviewcolumn.set_spacing(100)
+        treeview.append_column(treeviewcolumn)
+        treeviewcolumn.pack_start(cellrenderertext, True)
+        treeviewcolumn.add_attribute(cellrenderertext, "text", 2)
+        
+        #column 3 of list view to display last modified time
+        treeviewcolumn = Gtk.TreeViewColumn("Modified Time")
+        treeviewcolumn.set_spacing(100)
+        treeview.append_column(treeviewcolumn)
+        treeviewcolumn.pack_start(cellrenderertext, True)
+        treeviewcolumn.add_attribute(cellrenderertext, "text", 3)
+        
+        #column 4 of list view to display size
+        treeviewcolumn = Gtk.TreeViewColumn("Size")
+        treeviewcolumn.set_spacing(100)
+        treeview.append_column(treeviewcolumn)
+        treeviewcolumn.pack_start(cellrenderertext, True)
+        treeviewcolumn.add_attribute(cellrenderertext, "text", 4)
+        
 
     def on_open_location(self, placessidebar, location, flags):
         location = placessidebar.get_location()
 
         print("Opened URI: %s" % (GLocalFile.get_uri(location)))
+        
+    
+    def get_files(self, store):
+        for file_name in os.listdir(CURRENT_DIRECTORY):
+            
+            modified_time = os.path.getmtime(CURRENT_DIRECTORY+file_name)
+            size = os.path.getsize(CURRENT_DIRECTORY+file_name)
+            if not file_name[0] == '.': 
+                if os.path.isdir(os.path.join(CURRENT_DIRECTORY, file_name)):
+                    store.append([
+                    Gtk.Image.new_from_stock(Gtk.STOCK_DIRECTORY, Gtk.IconSize.MENU), 
+                    file_name, 
+                    True,
+                    modified_time,
+                    size
+                    ])
+                else:
+                    store.append([
+                    Gtk.Image.new_from_stock(Gtk.STOCK_FILE, Gtk.IconSize.MENU), 
+                    file_name, 
+                    False,
+                    modified_time,
+                    size
+                    ])
+                    
+                    
+    def on_button1_clicked(self, widget, event):
+        if event.button == 1:
+            print("Hello")
+        elif event.button == 3:
+            print("welcome")
         
 
 win = EntryWindow()
